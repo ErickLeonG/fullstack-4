853d46f24b96d6001e0a107d0d8ec98abbe2b44f
Erick Leon <erick.leon.gomez.03@gmail.com>
2025-12-07 01:50:53 +0200

    4.5

---
+++ utils/list_helper.js
@@ -0,0 +1,25 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.reduce((fav, blog) => (fav.likes >= blog.likes ? fav : blog), blogs[0])
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+}
+
